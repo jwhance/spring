23f963715ab33956b762c3a7853ec378820dfaea
jwhance <jwh20@hotmail.com>
2023-09-27 14:52:44 -0400

    Initial

---
+++ src/main/java/com/example/spring/Application.java
@@ -0,0 +1,24 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//
+// https://medium.com/@nadinCodeHat/spring-web-services-soap-443c5ed6b224
+//
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(Application.class);
+    }
+
+}
